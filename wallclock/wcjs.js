4b7256673f8f219bb9d3afbd2a89be4677d0f8cd
AjuArjun <arjunjeya9475@gmail.com>
2023-05-31 21:25:32 +0530

    first commit

---
+++ wallclock/wcjs.js
@@ -0,0 +1,20 @@
+let hours=document.getElementById('hours');
+hours.innerHTML="22"
+hours.style.display="flex"
+hours.style.justifyContent="center";
+hours.style.alignItems="center"
+
+let mins=document.getElementById('mins');
+mins.innerHTML="7"
+mins.style.display="flex"
+mins.style.justifyContent="center";
+mins.style.alignItems="center"
+
+let sec=document.getElementById('sec');
+sec.innerHTML="94"
+sec.style.display="flex"
+sec.style.justifyContent="center";
+sec.style.alignItems="center"
+
+let tir=Date.hours;
+console.log(tir)
